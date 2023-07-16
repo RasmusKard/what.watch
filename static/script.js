3b756f838e764c08e6302a0a8d5d301ee25c2cc9
RasmusKard <rasmuskard@hotmail.com>
2023-07-16 17:03:23 +0300

    Added touch and drag support for the sliders.

--- static/script.js
+++ static/script.js
@@ -16,57 +16,96 @@ $(document).ready(function() {
   });
 
   // Rating Range Slider
-  $("#ratingRangeSliderContainer").slider({
-    range: true,
-    min: 0,
-    max: 10,
+  var ratingRangeSlider = document.getElementById('ratingRangeSliderContainer');
+  var ratingRangeValue = document.getElementById('ratingRangeValue');
+  var minRatingInput = document.getElementById('minRatingInput');
+  var maxRatingInput = document.getElementById('maxRatingInput');
+
+  noUiSlider.create(ratingRangeSlider, {
+    start: [0, 10],
+    connect: true,
     step: 0.1,
-    values: [0, 10],
-    slide: function(event, ui) {
-      $("#ratingRangeValue").text(ui.values[0].toFixed(1) + " - " + ui.values[1].toFixed(1));
-      $("#minRatingInput").val(ui.values[0].toFixed(1));
-      $("#maxRatingInput").val(ui.values[1].toFixed(1));
+    range: {
+      'min': 0,
+      'max': 10
     }
   });
-  var ratingSliderValues = $("#ratingRangeSliderContainer").slider("values");
-  $("#ratingRangeValue").text(ratingSliderValues[0].toFixed(1) + " - " + ratingSliderValues[1].toFixed(1));
-  $("#minRatingInput").val(ratingSliderValues[0].toFixed(1));
-  $("#maxRatingInput").val(ratingSliderValues[1].toFixed(1));
+
+  ratingRangeSlider.noUiSlider.on('update', function(values, handle) {
+    var minValue = parseFloat(values[0]);
+    var maxValue = parseFloat(values[1]);
+
+    ratingRangeValue.textContent = minValue.toFixed(1) + ' - ' + maxValue.toFixed(1);
+    minRatingInput.value = minValue.toFixed(1);
+    maxRatingInput.value = maxValue.toFixed(1);
+  });
+
 
   // Minimum Amount of Votes Slider
-  $("#minVotesSlider").slider({
-    range: "min",
-    min: 0,
-    max: 250000, // Update the max value as needed
-    value: 0,
-    step: 25, // Update the step value as needed
-    slide: function (event, ui) {
-      var formattedValue = ui.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      $("#minVotesValue").text(formattedValue);
-      $("#minVotesInput").val(ui.value);
+  var minVotesSlider = document.getElementById('minVotesSlider');
+  var minVotesValue = document.getElementById('minVotesValue');
+  var minVotesInput = document.getElementById('minVotesInput');
+
+  noUiSlider.create(minVotesSlider, {
+    range: {
+      'min': 0,
+      'max': 250000
     },
-  });
-
-  var minVotesValue = $("#minVotesSlider").slider("value");
-  $("#minVotesValue").text(minVotesValue);
-  $("#minVotesInput").val(minVotesValue);
-
-  // Release Year Range Slider
-  $("#yearSlider").slider({
-    range: true,
-    min: 1900,
-    max: 2023,
-    values: [1900, 2023],
-    slide: function(event, ui) {
-      $("#yearRangeValue").text(ui.values[0] + " - " + ui.values[1]);
-      $("#minYearInput").val(ui.values[0]);
-      $("#maxYearInput").val(ui.values[1]);
+    step: 25,
+    start: 0,
+    format: {
+      to: function (value) {
+        return value.toString();
+      },
+      from: function (value) {
+        return Number(value.replace(/,/g, ''));
+      }
     }
   });
-  var yearSliderValues = $("#yearSlider").slider("values");
-  $("#yearRangeValue").text(yearSliderValues[0] + " - " + yearSliderValues[1]);
-  $("#minYearInput").val(yearSliderValues[0]);
-  $("#maxYearInput").val(yearSliderValues[1]);
-});
+
+
+  minVotesSlider.noUiSlider.on('update', function (values, handle) {
+    var floatValue = parseFloat(values[handle]);
+    var formattedValue = floatValue.toLocaleString(undefined, { maximumFractionDigits: 0 });
+    minVotesValue.textContent = formattedValue;
+    minVotesInput.value = floatValue;
+  });
+
+
+
+
+
+
+
+
+
+
+  // Release Year Range Slider
+  var yearSlider = document.getElementById('yearSlider');
+  var yearRangeValue = document.getElementById('yearRangeValue');
+  var minYearInput = document.getElementById('minYearInput');
+  var maxYearInput = document.getElementById('maxYearInput');
+
+  noUiSlider.create(yearSlider, {
+    start: [1900, 2023],
+    connect: true,
+    step: 1,
+    range: {
+      'min': 1900,
+      'max': 2023
+    }
+  });
+
+  yearSlider.noUiSlider.on('update', function(values, handle) {
+    var minYear = Math.round(values[0]);
+    var maxYear = Math.round(values[1]);
+
+    yearRangeValue.textContent = minYear + ' - ' + maxYear;
+    minYearInput.value = minYear;
+    maxYearInput.value = maxYear;
+  });
+})
+
+
 
 
