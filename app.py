91a9f1c664e9d4e3f6e8cea34151381e7917a85f
RasmusKard <rasmuskard@hotmail.com>
2023-08-01 16:39:17 +0300

    Added total number of possibilities for randomization and probability percentage of rolling the same thing again given the current dataset. Improved IMDb ID textbox appearance.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 from flask import Flask, render_template, send_from_directory
 import os
 from flask_modules import get_sorted_data, get_poster_url
+from decimal import Decimal, getcontext
 
 app = Flask(__name__)
 app.debug = True
@@ -23,7 +24,7 @@ def serve_static(filename):
 
     # Check if the requested path is within the static folder
     if not requested_path.startswith(static_folder_path):
-        # If the requested path is outside of the static folder, return an error or handle it as needed
+        # If the requested path is outside the static folder, return an error or handle it as needed
         return "Invalid file path", 400
 
     # If the path is safe, serve the file using send_from_directory
@@ -32,17 +33,26 @@ def serve_static(filename):
 
 @app.route('/run_script', methods=['POST'])
 def run_script():
-
     sorted_data = get_sorted_data()
-    
-    if len(sorted_data) == 0:
+    global row_count
+    row_count = len(sorted_data.index)
+
+    if row_count == 0:
         # If the sorted data is empty, return an error
         error_message = "Error: No results found, please widen search parameters."
         return render_template("index.html", error_message=error_message), 400
     randomized_data = sorted_data.sample()
     poster_url, overview = get_poster_url(randomized_data['tconst'].values[0])
+
+    getcontext().prec = 3
+    global probability
+    probability = Decimal('100') / Decimal(f'{row_count}')
+
+    # Thousand separators
+    row_count = '{:,}'.format(row_count)
     
-    return render_template("randomized_content.html", sorted_data=randomized_data, poster_url=poster_url, overview=overview)
+    return render_template("randomized_content.html", sorted_data=randomized_data, poster_url=poster_url,
+                           overview=overview, row_count=row_count, probability=probability)
 
 @app.route('/reroll', methods=['POST'])
 def reroll():
@@ -51,7 +61,8 @@ def reroll():
     else:
         randomized_data = get_sorted_data().sample()
     poster_url, overview = get_poster_url(randomized_data['tconst'].values[0])
-    return render_template("randomized_content.html", sorted_data=randomized_data, poster_url=poster_url, overview=overview)
+    return render_template("randomized_content.html", sorted_data=randomized_data, poster_url=poster_url,
+                           overview=overview, row_count=row_count, probability=probability)
     
 
 
