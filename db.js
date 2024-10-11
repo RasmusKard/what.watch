47e41c478f99ef58f542218ce8c60ebdae55b67f
RasmusKard <rasmuskard@gmail.com>
2024-10-11 18:27:36 +0300

    temp change vars

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const dbConfig = {
 	client: "mysql2",
 	connection: {
 		host: "db",
-		port: process.env.DB_PORT,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE_NAME,
+		port: 3306,
+		user: root,
+		password: 1234,
+		database: "dataset_sql",
 	},
 };
 
