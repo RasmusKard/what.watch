7836a08930226b8f6c8ad8e2e7b40902a54fc131
RasmusKard <rasmuskard@gmail.com>
2024-10-11 13:55:55 +0300

    change: use native docker health test instead of bash script

--- playwright.config.js
+++ playwright.config.js
@@ -49,32 +49,32 @@ defineConfig({
 		},
 
 		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
+		{
+			name: "Mobile Chrome",
+			use: { ...devices["Pixel 5"] },
+		},
+		{
+			name: "Mobile Safari",
+			use: { ...devices["iPhone 12"] },
+		},
 
 		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		{
+			name: "Microsoft Edge",
+			use: { ...devices["Desktop Edge"], channel: "msedge" },
+		},
+		{
+			name: "Google Chrome",
+			use: { ...devices["Desktop Chrome"], channel: "chrome" },
+		},
 	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+		command: "node server.js",
+		port: 3000,
+		reuseExistingServer: !process.env.CI,
+	},
 });
 
 export { defineConfig };
