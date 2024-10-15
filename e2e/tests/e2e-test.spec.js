89d46519da3ed0ea07af9df3fb390d764d475629
RasmusKard <rasmuskard@gmail.com>
2024-10-15 18:03:09 +0300

    try to fail tests

--- e2e/tests/e2e-test.spec.js
+++ e2e/tests/e2e-test.spec.js
@@ -41,7 +41,6 @@ test("test", async ({ page, browserName }) => {
 			Math.floor(Math.random() * availableGenres.length),
 			1
 		);
-		// await page.getByText(splicedElement).click({ force: true });
 		await page
 			.locator(`label[for="${splicedElement}"]`)
 			.click({ force: isWebkit });
@@ -67,7 +66,7 @@ test("test", async ({ page, browserName }) => {
 	const allowedMinRating = await page.evaluate(() => {
 		const ratingNum = Math.floor(Math.random() * 80) / 10;
 		const ratingSlider = document.getElementById("rating-slider");
-		ratingSlider.noUiSlider.set(ratingNum);
+		ratingSlider.noUiSlider.set(11);
 
 		return ratingNum;
 	});
